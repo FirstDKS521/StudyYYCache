63a2839d2daee3044d12e940270e33026d40feb7
duks <duks@robooot.com>
2017-02-13 17:09:23 +0800

    Initial Commit

---
+++ StudyYYCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyYYCache
+//
+//  Created by aDu on 2017/2/13.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
